237cf3c3c6e81e77f8685dbb6f7d900cfa299abb
VinhPhamAI <viymhg1234@gmail.com>
2024-08-12 13:43:20 +0700

    fix some bug

--- ecommerce_app/website/urls.py
+++ ecommerce_app/website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.authencation, name="authencation"),
-    path('', )
+    #path('', views.authencation, name="authencation"),
+    path('', views.index, name="index")
 ]
--- ecommerce_app/website/views.py
+++ ecommerce_app/website/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
-def authencation(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-        else:
-            form = UserCreationForm()
-    return render(request, 'sign_up.html', {'form' : form})
+# def authencation(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST or None)
+#         if form.is_valid():
+#             form.save()
+#         else:
+#             form = UserCreationForm()
+#     return render(request, 'sign_up.html', {'form' : form})
 
 # Create your views here.
 def index(request):
