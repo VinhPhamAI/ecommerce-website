770a957136ef89bb0e3e5a4e18caefe15bef5b63
VinhPhamAI <viymhg1234@gmail.com>
2024-08-12 11:47:49 +0700

    merge

--- ecommerce_app/website/views.py
+++ ecommerce_app/website/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def authencation(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        else:
+            form = UserCreationForm()
+    return render(request, 'sign_up.html', {'form' : form})
 
 # Create your views here.
-def index(request):
-    return render(request, 'landing_page.html')
+@login_required
+def landing_page(request):
+    return render(request, 'landing_page.html', {})
