a210b17b60ebd06b61741bad1a33ec6fd0d08a2c
VinhPhamAI <viymhg1234@gmail.com>
2024-08-19 13:25:47 +0700

    fix book_detail bug database

--- ecommerce_app/website/views.py
+++ ecommerce_app/website/views.py
@@ -32,38 +32,6 @@ def landing_page(request):
     # Chọn 10 sách ngẫu nhiên
     random_books = random.sample(all_books, min(10, len(all_books)))
 
-    # Đảm bảo thư mục lưu hình ảnh tồn tại
-    img_dir = os.path.join('static', 'image')
-    if not os.path.exists(img_dir):
-        os.makedirs(img_dir)
-
-    # Tải và lưu hình ảnh
-    for book in random_books:
-        if book.image_url_l:
-            try:
-                # Thêm tiêu đề HTTP `User-Agent`
-                headers = {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-                }
-                response = requests.get(book.image_url_l, headers=headers)
-                response.raise_for_status()
-
-                img = Image.open(BytesIO(response.content))
-
-                if img.mode != 'RGB':
-                    img = img.convert('RGB')
-
-                img_name = f"{book.isbn}.jpg"  # Tạo tên file dựa trên ISBN
-                img_path = os.path.join(img_dir, img_name)
-                img.save(img_path)
-
-                # Cập nhật đường dẫn hình ảnh trong cơ sở dữ liệu
-                book.new_column = img_path
-                book.save()
-            except Exception as e:
-                # Ghi lại lỗi và tiếp tục với sách tiếp theo
-                print(f"Error loading image for book '{book.title}': {e}")
-
     # Render trang với danh sách sách
     return render(request, 'landing_page.html', {'books': random_books})
 
@@ -146,5 +114,6 @@ def add_product(request):
 def book_detail(request, isbn):
     # Truy vấn sách từ cơ sở dữ liệu dựa trên ISBN
     book = get_object_or_404(Book, isbn=isbn)
+    print(book)
     # Truyền sách vào template
     return render(request, 'book_detail.html', {'book': book})
